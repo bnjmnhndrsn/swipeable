c6f509096846e87109ab419d44dd4a70217c72d9
Ben <benjamin.c.henderson@gmail.com>
2014-12-19 23:29:54 -0800

    detects left/right placement

--- swipable.js
+++ swipable.js
@@ -1,4 +1,9 @@
 var swipable = function($target){
 	$target.addClass("swipable-container");
-	$target.children().addClass("swipable-element");
+	$target.children().addClass("swipable-element").draggable().on("drag", function(){
+		var left = $(this).css("left");
+		$(this).removeClass("left right");
+		$(this).addClass(parseInt(left.replace("px", "")) > 0 ? "left" : "right");
+	});
+	
 }
